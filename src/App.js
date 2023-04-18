45da4cc4ca90dfb423e5e9dcafc33f2455ccc1ae
Martin <martinasman01@gmail.com>
2023-04-19 07:02:25 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+export default function App() {
+
+  return (
+    <model-viewer 
+    id="glb"
+    src="/assets/Handpaint_-_Girl_01.glb" 
+    ios-src="/assets/Handpaint_-_Girl_01.usdz"
+    shadow-intensity="1" 
+    autoplay    
+    ar camera-controls touch-action="pan-y" 
+    >     
+    </model-viewer>
+  );
+}
